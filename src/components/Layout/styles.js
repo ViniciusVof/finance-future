6267f9f3984480cbf63d3e89bfc37aad539f60cd
Vinicius Oliveira de Freitas <vini@MacBook-Air-de-Vinicius.local>
2023-02-03 11:48:08 -0300

    fix(overflow): fix overflow-y

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   display: flex;
   min-height: 100vh;
-  width: 100vw;
 `;
 export const Content = styled.section`
   display: flex;
